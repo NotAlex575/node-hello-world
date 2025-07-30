ae4afd3e37dea02f65c47ebd8d67d75aefb94186
NotAlex575 <dapontealessandro89gmail.com>
2025-07-30 14:09:04 +0200

    creato consoleLogModify, che contiene una function che ritorna una stringa inserita li + input dell'utente, inserito l'export per permettere di richiamarlo in index.js, dichiarato il require per passare i valori da index all'altro js, ottenimento della stringa completa con il console.log

---
+++ consoleLogModify.js
@@ -0,0 +1,5 @@
+const cosaScrivo = (output) =>{
+    return "ecco l'output uscito con il tuo input: "+output;
+}
+
+module.exports = cosaScrivo;
--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-console.log("hello worlds!");
+//NON EXTRA
+//console.log("hello worlds!");
 //ESECUZIONE TERMINAL: node index.js
+
+//EXTRA
+
+const cosaScrivo = require("./consoleLogModify.js");
+
+const outputConsole = process.argv[2];
+const output = cosaScrivo(outputConsole);
+
+console.log(output);
